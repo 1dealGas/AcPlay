c3234ce2da4cd1eeecc14d4c9dd53a6030c5bc9c
Fux <109365408+Fuxfantx@users.noreply.github.com>
2023-12-17 02:08:57 +0800

    20231217

--- AcArf2/src/libarf2.cpp
+++ AcArf2/src/libarf2.cpp
@@ -71,8 +71,9 @@ static inline void GetSINCOS(const double degree) {
 
 
 // Input Functions
-typedef dmVMath::Vector3* v3p;
-static inline bool has_touch_near( const uint64_t hint, const v3p touches[10] ) {
+static dmVMath::Vector3* T[10];
+typedef dmVMath::Vector3 v3, *v3p;
+static inline bool has_touch_near(const uint64_t hint) {
 
 	// Hint: (1)TAG+(19)judged_ms+(19)ms+(12)y+(13)x
 	// x:[0,48]   visible -> [16,32]
@@ -92,8 +93,8 @@ static inline bool has_touch_near( const uint64_t hint, const v3p touches[10] ) 
 	// For each finger,
 	for( uint8_t i=0; i<10; i++ ){
 
-		v3p f = touches[i];
-		uint8_t fs = (uint8_t)( f->getZ() );
+		v3p f = T[i];
+		uint8_t fs = (uint8_t)( f->getZ() );   // s0->invalid, s1->pressed, s2->onscreen, s3->released
 
 		// if the finger is just pressed, or onscreen,
 		if ( fs==1 || fs==2 ){
@@ -202,6 +203,7 @@ static inline int InitArf(lua_State *L)
 	return 4;
 }
 
+
 // UpdateArf(mstime, table_w/wi/h/hi/ht/a/ai/at) -> hint_lost, wgo/hgo/ago_used
 static inline int UpdateArf(lua_State *L)
 {S
@@ -219,45 +221,33 @@ static inline int UpdateArf(lua_State *L)
 	return 4;
 }
 
-// JudgeArf(mstime, idelta, table_touch) -> hint_hit, hint_lost, special_hint_judged
+
+// Recommended Usage:
+//     local v3,T = vmath.vector3(), Arf2.NewTable(10,0);    for i=1,10 do T[i]=v3() end
+//     Arf2.SetTouches( T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8], T[9], T[10] )
+static inline int SetTouches(lua_State *L) {
+	for( uint8_t i=0; i<10; i++)  T[i] = dmScript::CheckVector3(L, i+1);
+	return 0;
+}
+
+// JudgeArf(mstime, idelta, any_pressed, any_released) -> hint_hit, hint_lost, special_hint_judged
 static inline int JudgeArf(lua_State *L)
 {S
-	// table_touch = { any_pressed, any_released, v(x,y,s)··· }
-	// s0->invalid, s1->pressed, s2->onscreen, s3->released
-	// fixed to contain 12 elements
 	double mstime = luaL_checknumber(L, 1);
-	int8_t min_dt = -37 + (int8_t)luaL_checknumber(L, 2);
-	int8_t max_dt = min_dt + 74;
+	int8_t min_dt = -37 + (int8_t)luaL_checknumber(L, 2);		int8_t max_dt = min_dt + 74;
+	bool any_pressed = lua_toboolean(L, 3);
+	if( lua_toboolean(L, 4) ) blnums.clear();   // Discard blocking conditions if any_released.
+	lua_pop(L, 4);
 
-	lua_pushinteger(L, 1);								// ··t  ->  1
-	lua_gettable(L, 3);									// ··t  ->  1  ->  ap
-	lua_pushinteger(L, 2);								// ··t  ->  1  ->  ap  ->  2
-	lua_gettable(L, 3);									// ··t  ->  1  ->  ap  ->  2  ->  ar
-
-	bool any_pressed = lua_toboolean(L, 5);
-	if( lua_toboolean(L, 7) ) blnums.clear();			// Discard blocking conditions if any_released.
-	lua_pop(L, 4);										// ··t
-
-	v3p touches[10];
-	for( lua_Integer i=3; i<13; i++ ) {
-		//	                                            // ··t
-		lua_pushinteger(L, i);                          // ··t  ->  i
-		lua_gettable(L, 3);                             // ··t  ->  i  ->  t[i]
-		touches[i-3] = dmScript::CheckVector3(L, 5);
-		lua_pop(L, 2);                                  // ··t
-	}
-	lua_pop(L, 3);										//
-
-
-	bool special_hint_judged;
-	lua_Number hint_hit, hint_lost;
-
-	// Acquired mstime, min/max dt, any pressed, touches[10] from Lua.
-	// Now we acquire some stuff from C++ logics.
+	// Acquired mstime, min/max dt, any pressed from Lua.
+	// Now we prepare returns, and then acquire some stuff from C++ logics.
+	bool special_hint_judged;					lua_Number hint_hit, hint_lost;
 	auto hint = Arf -> mutable_hint();			auto idx_size = Arf -> index() -> size();
 	uint64_t _group = (uint64_t)mstime >> 9;	uint16_t which_group = _group>1 ? _group-1 : 0 ;
 			 _group = which_group + 3;			uint16_t byd1_group = _group<idx_size ? _group : idx_size;
 
+
+	// Start Judging.
 	if(any_pressed){
 
 		uint32_t min_time = 0;
@@ -278,8 +268,8 @@ static inline int JudgeArf(lua_State *L)
 				if(dt <- 510.0f) break;
 				if(dt >  470.0f) continue;
 				///
-				bool htn = has_touch_near(current_hint, touches);
 				uint8_t ch_status = HStatus(current_hint);
+				bool htn = has_touch_near(current_hint);
 
 				// For non-judged hints,
 				if( ch_status<10 ) {   // HINT_NONJUDGED
@@ -288,7 +278,7 @@ static inline int JudgeArf(lua_State *L)
 							bool checker_true = is_safe_to_anmitsu(current_hint);
 
 							// for earliest Hint(s) valid in this touch_press,
-							if( !min_time || min_time==hint_time ){
+							if( !min_time ){
 
 								min_time = hint_time;
 								if( dt>=min_dt && dt<=max_dt )	hint_hit += 1;
@@ -301,7 +291,8 @@ static inline int JudgeArf(lua_State *L)
 								if( current_hint_id == special_hint )
 								special_hint_judged = (bool)special_hint;
 							}
-							else if( checker_true ){   // for other Hint(s) valid and safe_to_anmitsu,
+							// for other Hint(s) valid and safe_to_anmitsu,
+							else if( hint_time==min_time || checker_true ){
 
 								if( dt>=min_dt && dt<=max_dt )	hint_hit += 1;
 								else							hint_lost += 1;
@@ -349,8 +340,8 @@ static inline int JudgeArf(lua_State *L)
 				if(dt <- 510.0f) break;
 				if(dt > 470.0f) continue;
 				///
-				bool htn = has_touch_near(current_hint, touches);
 				uint8_t ch_status = HStatus(current_hint);
+				bool htn = has_touch_near(current_hint);
 
 				if( ch_status<10 ) {   // HINT_NONJUDGED
 					if(htn) hint -> Mutate( current_hint_id, (current_hint<<1) >> 1 + 0x8000000000000000 );
@@ -363,7 +354,7 @@ static inline int JudgeArf(lua_State *L)
 		}
 	}
 
-	DM_LUA_STACK_CHECK(L, 3);
+	// No need to check the stack size since we popped 4 Lua values.
 	lua_pushnumber(L, hint_hit);
 	lua_pushnumber(L, hint_lost);
 	lua_pushboolean(L, special_hint_judged);
@@ -401,7 +392,8 @@ static inline int NewTable(lua_State *L) {
 // Defold Lifecycle Related Stuff
 static const luaL_reg M[] =
 {
-	{"InitArf", InitArf}, {"UpdateArf", UpdateArf}, {"JudgeArf", JudgeArf}, {"FinalArf", FinalArf},
+	{"InitArf", InitArf}, {"UpdateArf", UpdateArf}, {"FinalArf", FinalArf},
+	{"SetTouches", SetTouches}, {"JudgeArf", JudgeArf},   // {"JudgeArfController", JudgeArfController},
 	{"SetXScale", SetXScale}, {"SetYScale", SetYScale},
 	{"SetXDelta", SetXDelta}, {"SetYDelta", SetYDelta},
 	{"SetRotDeg", SetRotDeg},
